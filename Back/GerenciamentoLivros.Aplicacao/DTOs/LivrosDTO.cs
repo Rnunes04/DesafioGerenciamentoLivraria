39b09d788a98f951f0927a247b0583b940eeb6b9
Rodrigo Nunes <rnunes@gvdasa.com.br>
2024-11-30 13:54:32 -0300

    Incluindo projeto, arquitetura, dbcontext, modelo e dto

---
+++ Back/GerenciamentoLivros.Aplicacao/DTOs/LivrosDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciamentoLivros.Aplicacao.DTOs
+{
+    public class LivrosDTO
+    {
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public string Genero { get; set; }
+        public int Ano { get; set; }
+    }
+}
---
+++ Back/GerenciamentoLivros.Infraestrutura/Dados/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using GerenciamentoLivros.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciamentoLivros.Infraestrutura.Dados
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){ }
+
+        public DbSet<Livros> Livros { get; set; }
+    }
+}
